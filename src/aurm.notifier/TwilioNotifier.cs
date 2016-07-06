b990bf6111da8d0c7c1e8bccc00efcd1a2a98d66
Colin <colinnlsn@gmail.com>
2016-07-05 20:05:24 -0500

    Renamed Class1 to TwilioNotifier
    This class needs serious work, but I do not yet know the API for Twilio.
    Leaving this in as a stub while I get the rest of the scaffolding setup.

--- src/aurm.notifier/TwilioNotifier.cs
+++ src/aurm.notifier/TwilioNotifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aurm.core.notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace aurm.notifier
 {
-    public class Class1
+    public class TwilioNotifier : INotifier
     {
+        public void Notify(Recipient rec, string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
