adfc7ac984865fc9c3eb3a92777dbff26524c619
Colin <colinnlsn@gmail.com>
2016-07-05 20:34:19 -0500

    Added another Unit Test for SMSRecipient
    Added a unit test that uses a wide variety of formats for string
    representations of phone numbers.

--- test/aurm.notifier.tests/SMSRecipientTests.cs
+++ test/aurm.notifier.tests/SMSRecipientTests.cs
@@ -51,5 +51,29 @@ namespace aurm.notifier.tests
                 SMSRecipient rec = new SMSRecipient(name, number);
             });
         }
+
+        [Theory]
+        [InlineData("3087774825")]
+        [InlineData("(281)388-0388")]
+        [InlineData("(281)388-0300")]
+        [InlineData("(979) 778-0978")]
+        [InlineData("(281)934-2479")]
+        [InlineData("(281)934-2447")]
+        [InlineData("(979)826-3273")]
+        [InlineData("(979)826-3255")]
+        [InlineData("1334714149")]
+        [InlineData("(281)356-2530")]
+        [InlineData("(281)356-5264")]
+        [InlineData("(936)825-2081")]
+        [InlineData("(832)595-9500")]
+        [InlineData("(832)595-9501")]
+        [InlineData("281-342-2452")]
+        [InlineData("1334431660")]
+        public void TestPhoneNumberTypes(string number)
+        {
+            string name = "";
+            SMSRecipient rec = new SMSRecipient(name, number);
+            Assert.Equal(rec.PhoneNumber, number);
+        }
     }
 }
