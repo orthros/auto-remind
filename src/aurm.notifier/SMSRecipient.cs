6bda27b0f44ea81c6885e660d81d634b26c3ab49
Colin <colinnlsn@gmail.com>
2016-07-05 19:57:58 -0500

    Added Notification System
    This is in the need of the most work.
    Added a dll, arum.notifier which has two classes TwilioNotifier, which
    will send SMSs to the Recipients and a SMSREcipient class.
    These need further implementation, but I have yet to get started
    learning the Twilio API.
    
    At the very least, I need to add unit tests for the SMSRecipient's
    constructor as it does Regex validation on the phone number passed

---
+++ src/aurm.notifier/SMSRecipient.cs
@@ -0,0 +1,30 @@
+﻿using aurm.core.notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aurm.core.utilities;
+using System.Text.RegularExpressions;
+
+namespace aurm.notifier
+{
+    public class SMSRecipient : Recipient
+    {
+        private const string _phoneRegexPattern = @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}";
+
+        public string PhoneNumber { get; private set; }
+
+        public SMSRecipient(string phoneNumber)
+        {
+            phoneNumber.ThrowIfNull(nameof(phoneNumber));
+
+            //Need to regex this out and make sure its a valid phone number
+            if(!Regex.IsMatch(phoneNumber,_phoneRegexPattern))
+            {
+                throw new ArgumentException($"The phone number: {phoneNumber} was not a valid phone number");
+            }
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
