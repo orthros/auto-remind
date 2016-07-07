71b0283e93981fb22fef73aaaf2a4bf7c35e0c71
Colin <colinnlsn@gmail.com>
2016-07-07 14:22:59 -0500

    Added a message to AurmTask
    Added string Message to AurmTask

--- src/aurm.core/tasks/AurmTask.cs
+++ src/aurm.core/tasks/AurmTask.cs
@@ -11,22 +11,31 @@ namespace aurm.core.tasks
     public class AurmTask
     {
         public AurmTask(Guid uiD, Recipient recipient, 
-                        TimeCondition timeCondition, List<ICondition> additionalConditions)
+                        string message, TimeCondition timeCondition, List<ICondition> additionalConditions)
         {
             recipient.ThrowIfNull(nameof(recipient));
             timeCondition.ThrowIfNull(nameof(timeCondition));
             additionalConditions.ThrowIfNull(nameof(additionalConditions));
-
+            
+            if(string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentException($"Parameter {nameof(message)} must not be null or whitespace", 
+                                            nameof(message));
+            }
             UiD = uiD;
             Recipient = recipient;
             TimeCondition = timeCondition;
             AdditionalConditions = additionalConditions;
+            Message = message;
 
             LastTriggeredTime = DateTime.MinValue;
         }
 
         public Guid UiD { get; private set; }
+
         public Recipient Recipient { get; private set; }
+
+        public string Message { get; set; }
 
         public TimeCondition TimeCondition { get; private set; }
 
